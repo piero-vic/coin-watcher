1ce24365844d7428d994aac3ac77d2254d5c4e07
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-12 13:39:43 -0500

    feat: Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-2xl font-bold">Crypto Dashboard</h1>
-      <p className="text-lg text-gray-500 font-semibold">
-        Simple dashboard that displays information about cryptocurrencies.
-      </p>
+      <Header />
     </>
   );
 };
