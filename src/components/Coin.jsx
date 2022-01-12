687d683edb48b3d730577db6f937156563d97e49
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-12 16:35:26 -0500

    feat: Format coin prices

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -6,12 +6,17 @@ const Coin = (props) => {
   return (
     <tr className="bg-gray-50">
       <td className="p-2 text-left">{market_cap_rank}</td>
-      <td className="p-2 text-right flex gap-2">
+      <td className="flex p-2 text-right gap-2">
         <img className="h-6 w-6" src={image} alt="" />
         <span>{symbol.toUpperCase()}</span>
       </td>
       <td className="p-2 text-right">
-        {`$${current_price.toLocaleString("en-US")}`}
+        {current_price.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 18,
+        })}
       </td>
     </tr>
   );
