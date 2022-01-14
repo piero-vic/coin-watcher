ea86c758ecbb81857daf2d24487906d75ec1682b
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-13 22:31:29 -0500

    feat: Add function to format monetary values

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -13,6 +13,22 @@ const Coin = (props) => {
     },
   } = props;
 
+  const formatValue = (number) => {
+    const options = {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    };
+
+    if (number === 1 || number % 1 !== 0) {
+      options.minimumFractionDigits = 2;
+      options.maximumFractionDigits = 18;
+    }
+
+    return number.toLocaleString("en-US", options);
+  };
+
   const priceChangeColor = (priceChange) =>
     priceChange > 0 ? "text-green-400" : "text-red-400";
 
@@ -25,19 +41,12 @@ const Coin = (props) => {
 
   return (
     <tr className="bg-gray-50">
-      <td className="text-xs p-2 text-right">{market_cap_rank}</td>
-      <td className="w-28 flex p-2 text-right gap-2">
+      <td className="p-2 text-xs text-right">{market_cap_rank}</td>
+      <td className="flex p-2 text-right w-28 gap-2">
         <img className="w-6 h-6" src={image} alt="" />
         <span>{symbol.toUpperCase()}</span>
       </td>
-      <td className="p-2 text-right">
-        {current_price.toLocaleString("en-US", {
-          style: "currency",
-          currency: "USD",
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 18,
-        })}
-      </td>
+      <td className="p-2 text-right">{formatValue(current_price)}</td>
       <td
         className={`p-2 text-right ${priceChangeColor(
           price_change_percentage_24h_in_currency
@@ -52,7 +61,7 @@ const Coin = (props) => {
       >
         {formatPriceChange(price_change_percentage_7d_in_currency)}
       </td>
-      <td className="p-2 text-right">{market_cap}</td>
+      <td className="p-2 text-right">{formatValue(market_cap)}</td>
     </tr>
   );
 };
