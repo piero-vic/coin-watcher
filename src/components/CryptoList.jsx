df1effe5f9a54850bca597e3d56e5388ee3e6b92
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-15 17:21:18 -0500

    feat: Change mobile design to show only name and price

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -11,27 +11,23 @@ const CryptoList = () => {
         if (error) return <div className="mx-auto">Error: {error.message}</div>;
         if (!isLoaded) return <div className="mx-auto">Loading...</div>;
         return (
-          <table className="w-full border-collapse">
-            <thead className="bg-gray-100">
-              <tr>
-                <th className="sticky left-0 p-2 text-xs text-right bg-gray-100">
-                  #
-                </th>
-                <th className="sticky p-2 text-xs text-left bg-gray-100 left-9">
-                  Coin
-                </th>
-                <th className="p-2 text-xs text-right">Price</th>
-                <th className="p-2 text-xs text-right">24h %</th>
-                <th className="p-2 text-xs text-right">7d %</th>
-                <th className="p-2 text-xs text-right">Market Cap</th>
-              </tr>
-            </thead>
-            <tbody>
+          <div className="w-full bg-gray-100 rounded-2xl p-4">
+            {/* HEADER */}
+            <div className="grid grid-cols-2 gap-4">
+              <div className="hidden p-2 text-xs text-right">#</div>
+              <div className="p-2 text-xs text-left">Coin</div>
+              <div className="p-2 text-xs text-right">Price</div>
+              <div className="hidden p-2 text-xs text-right">24h %</div>
+              <div className="hidden p-2 text-xs text-right">7d %</div>
+              <div className="hidden p-2 text-xs text-right">Market Cap</div>
+            </div>
+            {/* BODY */}
+            <div>
               {data.map((coinData) => (
                 <Coin coinData={coinData} key={coinData.id} />
               ))}
-            </tbody>
-          </table>
+            </div>
+          </div>
         );
       })()}
     </div>
