113d18ec3d7012a80baef54bdd570a34d55c0ffa
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-15 12:28:47 -0500

    refactor: Store request parameters in a variable

--- src/hooks/useCryptoData.js
+++ src/hooks/useCryptoData.js
@@ -2,13 +2,21 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useCryptoData = () => {
-  const URL =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d";
+  const URL = "https://api.coingecko.com/api/v3/coins/markets";
 
   const REQUEST_HEADER = {
     "Cache-Control": "no-cache",
     Pragma: "no-cache",
     Expires: "0",
+  };
+
+  const REQUEST_PARAMS = {
+    vs_currency: "usd",
+    order: "market_cap_desc",
+    per_page: 100,
+    page: 1,
+    sparkline: true,
+    price_change_percentage: "1h,24h,7d",
   };
 
   const [data, setData] = useState([]);
@@ -17,7 +25,10 @@ const useCryptoData = () => {
 
   const getData = async () => {
     try {
-      const response = await axios.get(URL, { headers: REQUEST_HEADER });
+      const response = await axios.get(URL, {
+        headers: REQUEST_HEADER,
+        params: REQUEST_PARAMS,
+      });
       setData(response.data);
     } catch (error) {
       setError(error);
