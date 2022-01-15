7e11e176332fad8cc375eb9e61d7afda8b95a3ff
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-15 12:17:12 -0500

    refactor: Use async/await with getData

--- src/hooks/useCryptoData.js
+++ src/hooks/useCryptoData.js
@@ -5,29 +5,24 @@ const useCryptoData = () => {
   const URL =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d";
 
+  const REQUEST_HEADER = {
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
+    Expires: "0",
+  };
+
   const [data, setData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
 
-  const getData = () => {
-    axios
-      .get(URL, {
-        headers: {
-          "Cache-Control": "no-cache",
-          Pragma: "no-cache",
-          Expires: "0",
-        },
-      })
-      .then(
-        (res) => {
-          setData(res.data);
-          setIsLoaded(true);
-        },
-        (error) => {
-          setError(error);
-          setIsLoaded(true);
-        }
-      );
+  const getData = async () => {
+    try {
+      const response = await axios.get(URL, { headers: REQUEST_HEADER });
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoaded(true);
   };
 
   useEffect(() => {
