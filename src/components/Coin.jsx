b79b3b064b0d85cd82a021c9b98ffefe5c63e7f9
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-15 13:21:19 -0500

    refactor: Move utility functions to utils directory

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  formatValue,
+  formatPriceChange,
+  priceChangeColor,
+} from "../utils/utils";
 
 const Coin = (props) => {
   const {
@@ -12,32 +17,6 @@ const Coin = (props) => {
       market_cap,
     },
   } = props;
-
-  const formatValue = (number) => {
-    const options = {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
-    };
-
-    if (number === 1 || number % 1 !== 0) {
-      options.minimumFractionDigits = 2;
-      options.maximumFractionDigits = 18;
-    }
-
-    return number.toLocaleString("en-US", options);
-  };
-
-  const priceChangeColor = (priceChange) =>
-    priceChange > 0 ? "text-green-400" : "text-red-400";
-
-  const formatPriceChange = (priceChange) =>
-    (priceChange / 100).toLocaleString("en-US", {
-      style: "percent",
-      minimumFractionDigits: 1,
-      maximumFractionDigits: 1,
-    });
 
   return (
     <tr className="bg-gray-50">
