143e717dd875e3ea506fc22c30e44e404f6c154b
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-15 18:13:27 -0500

    feat: Add max width to crypto chart

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -6,12 +6,12 @@ const CryptoList = () => {
   const { data, isLoaded, error } = useCryptoData();
 
   return (
-    <div className="flex items-center py-5 mx-5 overflow-auto grow">
+    <div className="grid place-items-center grow">
       {(() => {
-        if (error) return <div className="mx-auto">Error: {error.message}</div>;
-        if (!isLoaded) return <div className="mx-auto">Loading...</div>;
+        if (error) return <div>Error: {error.message}</div>;
+        if (!isLoaded) return <div>Loading...</div>;
         return (
-          <div className="w-full p-4 bg-gray-100 rounded-2xl">
+          <div className="w-11/12 p-4 my-5 bg-gray-100 max-w-screen-lg rounded-2xl">
             {/* HEADER */}
             <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4">
               <div className="hidden p-2 text-xs text-right sm:block">#</div>
