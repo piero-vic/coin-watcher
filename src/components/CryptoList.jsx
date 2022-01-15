7a0b772ff5c72c19e09d93101035d28c12963f87
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-15 17:36:58 -0500

    feat: Add responsiveness to the cyrpto list

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -11,15 +11,19 @@ const CryptoList = () => {
         if (error) return <div className="mx-auto">Error: {error.message}</div>;
         if (!isLoaded) return <div className="mx-auto">Loading...</div>;
         return (
-          <div className="w-full bg-gray-100 rounded-2xl p-4">
+          <div className="w-full p-4 bg-gray-100 rounded-2xl">
             {/* HEADER */}
-            <div className="grid grid-cols-2 gap-4">
-              <div className="hidden p-2 text-xs text-right">#</div>
+            <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4">
+              <div className="hidden p-2 text-xs text-right sm:block">#</div>
               <div className="p-2 text-xs text-left">Coin</div>
               <div className="p-2 text-xs text-right">Price</div>
-              <div className="hidden p-2 text-xs text-right">24h %</div>
-              <div className="hidden p-2 text-xs text-right">7d %</div>
-              <div className="hidden p-2 text-xs text-right">Market Cap</div>
+              <div className="hidden p-2 text-xs text-right sm:block">
+                24h %
+              </div>
+              <div className="hidden p-2 text-xs text-right md:block">7d %</div>
+              <div className="hidden p-2 text-xs text-right lg:block">
+                Market Cap
+              </div>
             </div>
             {/* BODY */}
             <div>
