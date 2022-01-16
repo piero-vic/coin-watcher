638b6fdb9afbfb4e76b8e4baabfc928d696b1ed0
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-15 23:49:21 -0500

    feat: Remove header background and center items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { HiDotsHorizontal } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <header className="flex justify-between p-5 bg-gray-400">
+    <header className="flex items-center justify-between p-5">
       <h1 className="text-lg font-bold">Crypto Dashboard</h1>
-      <button className="p-2 text-xl border border-white rounded-2xl hover:border-gray-500">
+      <button className="p-2 text-xl border border-white-100 rounded-2xl hover:border-gray-400">
         <HiDotsHorizontal />
       </button>
     </header>
