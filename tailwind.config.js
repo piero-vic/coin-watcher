2f67bea75b8931c3daee712032bcfa1877dc4401
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-16 17:51:06 -0500

    build(style): Add Inter font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         5: "20px 1.5fr repeat(3, 1fr)",
         6: "20px 3fr repeat(4, 1fr)",
       },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [],
