148156c44e4f16609faef2b79d0ecb5b988d314d
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-15 23:42:45 -0500

    feat: Add menu button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { HiDotsHorizontal } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <header className="flex p-5 bg-gray-400">
+    <header className="flex justify-between p-5 bg-gray-400">
       <h1 className="text-lg font-bold">Crypto Dashboard</h1>
+      <button className="p-2 text-xl border border-white rounded-2xl hover:border-gray-500">
+        <HiDotsHorizontal />
+      </button>
     </header>
   );
 };
