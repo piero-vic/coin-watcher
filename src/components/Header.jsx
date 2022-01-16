9e737d2f53c78e8f9f386acde93ed99cf2fec8ca
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-16 00:41:27 -0500

    feat: Add dropdown menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import DropdownMenu from './DropdownMenu'
 import { HiDotsHorizontal } from "react-icons/hi";
 
 const Header = () => {
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  const handleButtonClick = () => {
+    if (displayMenu) {
+      setDisplayMenu(false);
+    } else {
+      setDisplayMenu(true);
+    }
+  };
+
   return (
     <header className="flex items-center justify-between p-5">
       <h1 className="text-lg font-bold">Crypto Dashboard</h1>
-      <button className="p-2 text-xl border border-white-100 rounded-2xl hover:border-gray-400">
-        <HiDotsHorizontal />
-      </button>
+      <div className="relative">
+        <button
+          className="p-2 text-xl border border-white-100 rounded-2xl hover:border-gray-400"
+          onClick={() => handleButtonClick()}
+        >
+          <HiDotsHorizontal />
+        </button>
+        {displayMenu && <DropdownMenu />}
+      </div>
     </header>
   );
 };
