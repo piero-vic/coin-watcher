cd70f7c38da5d496909e8bd068a51d80e71b8875
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-16 06:31:59 -0500

    refactor: Move menu button to DropdownMenu
    This cleans up the Header component. DropdownMenu component now contains
    all thigs related to the toggle of the menu.

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,28 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { HiDotsHorizontal } from "react-icons/hi";
 import { AiOutlineInfoCircle, AiOutlineGithub } from "react-icons/ai";
 import { BsSun } from "react-icons/bs";
 import { IoDocumentTextOutline } from "react-icons/io5";
 
 const DropdownMenu = () => {
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  const handleButtonClick = () => {
+    if (displayMenu) {
+      setDisplayMenu(false);
+    } else {
+      setDisplayMenu(true);
+    }
+  };
+
+  const menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (event) => {
+      if (!menuRef.current.contains(event.target)) setDisplayMenu(false);
+    };
+
+    document.addEventListener("mousedown", handler);
+
+    return () => document.removeEventListener("mousedown", handler);
+  });
+
   return (
-    <span className="absolute right-0 flex flex-col w-56 p-3 bg-white border top-12 border-white-100 rounded-2xl drop-shadow-xl">
-      <a href="" className="flex items-center justify-between p-1">
-        <div>About</div>
-        <AiOutlineInfoCircle />
-      </a>
-      <a href="" className="flex items-center justify-between p-1">
-        <div>GitHub</div>
-        <AiOutlineGithub />
-      </a>
-      <a href="" className="flex items-center justify-between p-1">
-        <div>Dark Theme</div>
-        <BsSun />
-      </a>
-      <a href="" className="flex items-center justify-between p-1">
-        <div>License</div>
-        <IoDocumentTextOutline />
-      </a>
-    </span>
+    <div ref={menuRef} className="relative">
+      <button
+        className="p-2 text-xl border border-white-100 rounded-2xl hover:border-gray-400"
+        onClick={() => handleButtonClick()}
+      >
+        <HiDotsHorizontal />
+      </button>
+      {displayMenu && (
+        <span className="absolute right-0 flex flex-col w-56 p-3 bg-white border top-12 border-white-100 rounded-2xl drop-shadow-xl">
+          <a href="" className="flex items-center justify-between p-1">
+            <div>About</div>
+            <AiOutlineInfoCircle />
+          </a>
+          <a href="" className="flex items-center justify-between p-1">
+            <div>GitHub</div>
+            <AiOutlineGithub />
+          </a>
+          <a href="" className="flex items-center justify-between p-1">
+            <div>Dark Theme</div>
+            <BsSun />
+          </a>
+          <a href="" className="flex items-center justify-between p-1">
+            <div>License</div>
+            <IoDocumentTextOutline />
+          </a>
+        </span>
+      )}
+    </div>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import DropdownMenu from "./DropdownMenu";
-import { HiDotsHorizontal } from "react-icons/hi";
 
 const Header = () => {
-  const [displayMenu, setDisplayMenu] = useState(false);
-
-  const handleButtonClick = () => {
-    if (displayMenu) {
-      setDisplayMenu(false);
-    } else {
-      setDisplayMenu(true);
-    }
-  };
-
-  const menuRef = useRef();
-
-  useEffect(() => {
-    let handler = (event) => {
-      if (!menuRef.current.contains(event.target)) setDisplayMenu(false);
-    };
-
-    document.addEventListener("mousedown", handler);
-
-    return () => document.removeEventListener("mousedown", handler);
-  });
-
   return (
     <header className="flex items-center justify-between p-5">
       <h1 className="text-lg font-bold">Crypto Dashboard</h1>
-      <div ref={menuRef} className="relative">
-        <button
-          className="p-2 text-xl border border-white-100 rounded-2xl hover:border-gray-400"
-          onClick={() => handleButtonClick()}
-        >
-          <HiDotsHorizontal />
-        </button>
-        {displayMenu && <DropdownMenu />}
-      </div>
+      <DropdownMenu />
     </header>
   );
 };
