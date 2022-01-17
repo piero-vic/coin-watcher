c2442139986fc38ed1018317a0558c9ce82b2008
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-17 17:53:59 -0500

    feat(style): Add functionality to toggle between themes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import Header from "./components/Header";
 import CryptoList from "./components/CryptoList";
 import Footer from "./components/Footer";
+import ThemeProvider from "./ThemeContext";
+import ThemeToggler from "./ThemeToggler";
 
 const App = () => {
   return (
-    <div className="dark">
-      <div className="flex flex-col min-h-screen font-medium bg-gradient-to-b from-cyan-100 to-cyan-50 dark:from-slate-800 dark:to-slate-700 dark:text-white">
-        <Header />
-        <CryptoList />
-        <Footer />
-      </div>
-    </div>
+    <ThemeProvider>
+      <ThemeToggler>
+        <div className="flex flex-col min-h-screen font-medium bg-gradient-to-b from-cyan-100 to-cyan-50 dark:from-slate-800 dark:to-slate-700 dark:text-white">
+          <Header />
+          <CryptoList />
+          <Footer />
+        </div>
+      </ThemeToggler>
+    </ThemeProvider>
   );
 };
 
