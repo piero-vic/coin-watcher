e896561beb1963c1dbf9b75400e1d3fb94055f98
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-16 22:32:56 -0500

    feat: Add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen font-medium bg-gradient-to-r from-cyan-100 to-blue-100">
-      <Header />
-      <CryptoList />
-      <Footer />
+    <div className="dark">
+      <div className="flex flex-col min-h-screen font-medium bg-gradient-to-b from-cyan-100 to-cyan-50 dark:from-slate-800 dark:to-slate-700 dark:text-white">
+        <Header />
+        <CryptoList />
+        <Footer />
+      </div>
     </div>
   );
 };
