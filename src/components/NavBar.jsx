31775c85c2a8940e092e762be2cafa4d4cf651c7
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-25 20:25:59 -0500

    feat(NavBar): Add active style to the NavLinks

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,36 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const inactiveLinkClasses = "py-1 px-3 rounded-2xl";
+  const activeLinkClasses =
+    "py-1 px-3 rounded-2xl bg-stone-100 dark:bg-zinc-800";
+
   return (
-    <nav className="flex px-4 py-2 bg-white border border-white gap-6 w-fit sm:justify-self-center dark:bg-zinc-900 dark:border-zinc-900 rounded-2xl">
-      <NavLink to="/">Chart</NavLink>
-      <NavLink to="/portfolio">Portfolio</NavLink>
-      <NavLink to="/news">News</NavLink>
+    <nav className="flex p-[2px] bg-white border border-white gap-2 w-fit sm:justify-self-center dark:bg-zinc-900 dark:border-zinc-900 rounded-2xl">
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? activeLinkClasses : inactiveLinkClasses
+        }
+        to="/"
+      >
+        Chart
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? activeLinkClasses : inactiveLinkClasses
+        }
+        to="/portfolio"
+      >
+        Portfolio
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? activeLinkClasses : inactiveLinkClasses
+        }
+        to="/news"
+      >
+        News
+      </NavLink>
     </nav>
   );
 };
