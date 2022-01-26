5762f60903db4432143ee55c07504e35a8f2fadd
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-25 19:32:16 -0500

    feat(routes): Setup routes and add navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import CryptoList from "./components/CryptoList";
 import Footer from "./components/Footer";
 import { ThemeContext } from "./contexts/ThemeContext";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -11,7 +12,25 @@ const App = () => {
     <main className={darkMode && "dark"}>
       <div className="flex flex-col min-h-screen font-medium bg-gradient-to-b from-cyan-100 to-cyan-50 dark:from-slate-800 dark:to-slate-700 dark:text-white">
         <Header />
-        <CryptoList />
+        <Routes>
+          <Route path="/" element={<CryptoList />} />
+          <Route
+            path="/portfolio"
+            element={
+              <div className="grid place-items-center grow">
+                <h1>Portfolio page under construction</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/news"
+            element={
+              <div className="grid place-items-center grow">
+                <h1>News page under construction</h1>
+              </div>
+            }
+          />
+        </Routes>
         <Footer />
       </div>
     </main>
