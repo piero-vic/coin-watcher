ce1b472c5ee50be88e9152bb7cb68936c5fff75a
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-25 19:32:16 -0500

    feat(routes): Setup routes and add navigation links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="flex px-4 py-2 bg-white border border-white gap-6 w-fit sm:justify-self-center dark:bg-zinc-900 dark:border-zinc-900 rounded-2xl">
-      <a href="/">Chart</a>
-      <a href="">Portfolio</a>
-      <a href="">News</a>
+      <NavLink to="/">Chart</NavLink>
+      <NavLink to="/portfolio">Portfolio</NavLink>
+      <NavLink to="/news">News</NavLink>
     </nav>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
