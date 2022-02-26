7c318be66d7e531a18c660de867c01dd3e40a77e
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-26 00:28:35 -0500

    feat(utils): Add functions to get API url

---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+export const CoinList = (currency) =>
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`;
+
+export const SingleCoin = (id) =>
+  `https://api.coingecko.com/api/v3/coins/${id}`;
