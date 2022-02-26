34b2f74a4d64202edd16358a02d8e3df8d2331a6
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-26 00:14:54 -0500

    feat(CoinDetails): Create basic structure

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { formatValue } from "../utils/utils";
 
 const CoinDetails = () => {
   const { coinId } = useParams();
@@ -30,7 +31,33 @@ const CoinDetails = () => {
       {(() => {
         if (error) return <div>Error: {error.message}</div>;
         if (!isLoaded) return <div>Loading...</div>;
-        return <h1>{`${coin.name} (${coin.symbol.toUpperCase()})`}</h1>;
+        return (
+          <div className="w-full place-self-start px-5">
+            {/* HEADER */}
+            <div className="mt-2">
+              <p className="flex items-center gap-2">
+                <img className="h-6 w-6" src={coin.image.small} alt="" />
+                <span>{coin.name}</span>
+                <span className="opacity-90">{`(${coin.symbol.toUpperCase()})`}</span>
+              </p>
+              <p className="flex items-baseline gap-2 text-2xl">
+                <span>{formatValue(coin.market_data.current_price.usd)}</span>
+                <span className="text-base">
+                  ({coin.market_data.price_change_percentage_24h})
+                </span>
+              </p>
+            </div>
+            {/* DATA */}
+            <div className="mt-4 flex flex-col md:flex-row gap-4">
+              <div className="w-full md:w-5/12 rounded-2xl bg-white p-4 drop-shadow-xl dark:bg-zinc-900">
+                This is where the data will be. 
+              </div>
+              <div className="w-full rounded-2xl bg-white p-4 drop-shadow-xl dark:bg-zinc-900">
+                This is where the chart will be. 
+              </div>
+            </div>
+          </div>
+        );
       })()}
     </div>
   );
