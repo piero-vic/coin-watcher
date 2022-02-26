73f20609d5e9c65276939a1e5297ad0dc77e773a
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-25 19:21:59 -0500

    feat(CoinDetails): Fetch data from API

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const CoinDetails = () => {
-  const { coinId } = useParams()
+  const { coinId } = useParams();
+
+  const [coin, setCoin] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchCoin = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/${coinId}`
+      );
+      setCoin(response.data);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoaded(true);
+  };
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
 
   return (
     <div className="grid grow place-items-center">
-      {`Coin details on ${coinId}.`}
+      {(() => {
+        if (error) return <div>Error: {error.message}</div>;
+        if (!isLoaded) return <div>Loading...</div>;
+        return <h1>{`${coin.name} (${coin.symbol.toUpperCase()})`}</h1>;
+      })()}
     </div>
-  )
-}
+  );
+};
 
 export default CoinDetails;
