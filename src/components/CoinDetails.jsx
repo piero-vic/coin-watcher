56bed1367b88de8c38874da0ab94094515b528d6
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-27 10:20:27 -0500

    feat(CoinDetails): Display coin market data

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -6,7 +6,6 @@ import { SingleCoin } from "../utils/api";
 
 const CoinDetails = () => {
   const { coinId } = useParams();
-
   const { data: coin, isLoaded, error } = useCryptoData(SingleCoin(coinId));
 
   return (
@@ -17,24 +16,52 @@ const CoinDetails = () => {
         return (
           <div className="w-full place-self-start px-5">
             {/* HEADER */}
-            <div className="mt-2">
-              <p className="flex items-center gap-2">
+            <div>
+              <p className="flex items-baseline gap-2">
                 <img className="h-6 w-6" src={coin.image.small} alt="" />
-                <span>{coin.name}</span>
-                <span className="opacity-90">{`(${coin.symbol.toUpperCase()})`}</span>
+                <span className="text-xl">{coin.name}</span>
+                <span className="opacity-60">{`${coin.symbol.toUpperCase()}`}</span>
               </p>
               <p className="flex items-baseline gap-2 text-2xl">
                 <span>{formatValue(coin.market_data.current_price.usd)}</span>
                 <span className="text-base">
-                  ({coin.market_data.price_change_percentage_24h})
+                  {coin.market_data.price_change_percentage_24h}
                 </span>
               </p>
             </div>
             {/* DATA */}
             <div className="mt-4 flex flex-col gap-4 md:flex-row">
-              <div className="w-full rounded-2xl bg-white p-4 drop-shadow-xl dark:bg-zinc-900 md:w-5/12">
-                This is where the data will be.
+              {/* INFO */}
+              <div className="flex w-full flex-col gap-2 rounded-2xl bg-white p-4 drop-shadow-xl dark:bg-zinc-900 md:w-5/12">
+                <div>
+                  <p className="font-medium opacity-60">Market Cap</p>
+                  <p>{formatValue(coin.market_data.market_cap.usd)}</p>
+                </div>
+                <div>
+                  <p className="font-medium opacity-60">24h Trading Vol</p>
+                  <p>{formatValue(coin.market_data.total_volume.usd)}</p>
+                </div>
+                {Object.keys(coin.market_data.fully_diluted_valuation)
+                  .length !== 0 ? (
+                  <div>
+                    <p className="font-medium opacity-60">Fully Diluted Valuation</p>
+                    <p>{formatValue(coin.market_data.fully_diluted_valuation.usd)} </p>
+                  </div>
+                ) : null}
+                <div>
+                  <p className="font-medium opacity-60">Circulating Supply</p>
+                  <p>{coin.market_data.circulating_supply?.toLocaleString("en-US")}</p>
+                </div>
+                <div>
+                  <p className="font-medium opacity-60">Total Supply</p>
+                  <p>{coin.market_data.total_supply?.toLocaleString("en-US") || "∞"}</p>
+                </div>
+                <div>
+                  <p className="font-medium opacity-60">Max Supply</p>
+                  <p>{coin.market_data.max_supply?.toLocaleString("en-US") || "∞"}</p>
+                </div>
               </div>
+              {/* CHART */}
               <div className="w-full rounded-2xl bg-white p-4 drop-shadow-xl dark:bg-zinc-900">
                 This is where the chart will be.
               </div>
