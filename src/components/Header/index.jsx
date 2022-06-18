b413f06afe59ff2a7be8bfdd7c98b0be9b7e0679
Piero Lescano <pierolescano@protonmail.com>
2022-06-18 01:20:47 -0500

    refactor: Replace ModalContext with useModal
    I'm getting rid of the ModalContext. This context was implemented in
    order to avoid prop drilling. I found out that composition is a much
    better solution so I'm using it on the Header component.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import DropdownMenu from "./DropdownMenu";
-import NavBar from "./NavBar";
 
-const Header = () => {
+const Header = ({ children }) => {
   return (
     <header className="grid grid-cols-2 items-center justify-between p-5 sm:grid-cols-3">
-      <a className="hidden w-min text-lg font-bold sm:block" href="/">
-        Coin Watcher
-      </a>
-      <NavBar />
-      <DropdownMenu />
+      {children}
     </header>
   );
 };
