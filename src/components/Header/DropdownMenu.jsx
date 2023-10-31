c02fc1793a0a7202743f46cc1c452a71eb644dd7
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2023-10-30 22:36:34 -0500

    feat: Add Headless UI Menu

--- src/components/Header/DropdownMenu.jsx
+++ src/components/Header/DropdownMenu.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import useToggleMenu from "../../hooks/useToggleMenu";
+import { Menu, Transition } from "@headlessui/react";
 import {
   FiFileText,
   FiInfo,
@@ -11,58 +11,63 @@ import { AiOutlineGithub } from "react-icons/ai";
 import { ThemeContext } from "../../contexts/ThemeContext";
 
 const DropdownMenu = ({ toggleModal }) => {
-  const [displayMenu, menuRef, toggleMenu] = useToggleMenu();
   const { darkMode, toggleDarkMode } = useContext(ThemeContext);
 
   return (
-    <div ref={menuRef} className="relative justify-self-end">
-      <button
-        className="rounded-2xl border border-white bg-white p-2 text-xl hover:border-gray-500 dark:border-zinc-900 dark:bg-zinc-900 dark:hover:border-gray-500"
-        onClick={toggleMenu}
-      >
+    <Menu as={"div"} className="relative justify-self-end">
+      <Menu.Button className="rounded-2xl border border-white bg-white p-2 text-xl hover:border-gray-500 dark:border-zinc-900 dark:bg-zinc-900 dark:hover:border-gray-500">
         <FiMoreHorizontal />
-      </button>
-      {displayMenu && (
-        <span className="absolute right-0 top-12 z-10 flex w-56 flex-col rounded-2xl bg-white p-3 drop-shadow-xl dark:bg-zinc-900">
-          <button
-            onClick={() => {
-              toggleMenu();
-              toggleModal("About");
-            }}
+      </Menu.Button>
+
+      <Transition
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
+        <Menu.Items className="absolute right-0 top-3 flex w-56 flex-col rounded-2xl bg-white p-3 drop-shadow-xl dark:bg-zinc-900">
+          <Menu.Item
+            as={"button"}
+            onClick={() => toggleModal("About")}
             className="flex items-center justify-between p-1 font-medium"
           >
-            <div>About</div>
-            <FiInfo />
-          </button>
-          <a
+            <span>About</span>
+            <FiInfo aria-hidden />
+          </Menu.Item>
+
+          <Menu.Item
+            as={"a"}
             href="https://github.com/piero-vic/crypto-dashboard"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center justify-between p-1"
           >
-            <div>GitHub</div>
-            <AiOutlineGithub />
-          </a>
-          <button
+            <span>GitHub</span>
+            <AiOutlineGithub aria-hidden />
+          </Menu.Item>
+
+          <Menu.Item
+            as={"button"}
             onClick={toggleDarkMode}
             className="flex items-center justify-between p-1 font-medium"
           >
-            <div>{darkMode ? "Light Theme" : "Dark Theme"}</div>
-            {darkMode ? <FiMoon /> : <FiSun />}
-          </button>
-          <button
-            onClick={() => {
-              toggleMenu();
-              toggleModal("MIT License");
-            }}
+            <span>{darkMode ? "Light Theme" : "Dark Theme"}</span>
+            {darkMode ? <FiMoon aria-hidden /> : <FiSun aria-hidden />}
+          </Menu.Item>
+
+          <Menu.Item
+            as={"button"}
+            onClick={() => toggleModal("MIT License")}
             className="flex items-center justify-between p-1 font-medium"
           >
-            <div>License</div>
-            <FiFileText />
-          </button>
-        </span>
-      )}
-    </div>
+            <span>License</span>
+            <FiFileText aria-hidden />
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 };
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = ({ children }) => {
   return (
-    <header className="grid grid-cols-2 items-center justify-between p-5 sm:grid-cols-3">
+    <header className="grid grid-cols-2 items-center justify-between p-5 sm:grid-cols-3 z-10">
       {children}
     </header>
   );
